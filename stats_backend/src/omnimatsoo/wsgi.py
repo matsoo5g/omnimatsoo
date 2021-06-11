1d4ce18440f41c4bdb6f26ebeb3f3776faecb8de
ChengYangWu <falau@users.noreply.github.com>
2021-06-12 00:05:45 +0200

    Turn on tornado general logs

--- stats_backend/src/omnimatsoo/wsgi.py
+++ stats_backend/src/omnimatsoo/wsgi.py
@@ -1,5 +1,8 @@
+import logging
 import os
+import sys
 
+import tornado.log
 from tornado import httpserver, wsgi, ioloop
 from tornado.netutil import bind_unix_socket
 
@@ -7,6 +10,12 @@ from omnimatsoo.app import create_app
 
 
 def start():
+    for logger_name in ("tornado.general",):
+        logger = logging.getLogger(logger_name)
+        logger.propagate = False
+        logger.setLevel(logging.INFO)
+        logger.addHandler(logging.StreamHandler(sys.stdout))
+
     app = create_app()
     http_server = httpserver.HTTPServer(wsgi.WSGIContainer(app))
     if socket_path := os.environ.get("APP_SOCKET_PATH"):
