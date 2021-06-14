d214d9d31de254b7998ef86334d66fa3c9b12499
ChengYangWu <falau@users.noreply.github.com>
2021-06-14 23:04:47 +0200

    Remove an unused entity

--- stats_backend/src/omnimatsoo/entities.py
+++ stats_backend/src/omnimatsoo/entities.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class DomEvent:
+    # see MediaEvents@ https://github.com/matsoo5g/omnimatsoo/blob/main/test_client/js/benchmarkcontroller.js
+    # ref: https://html.spec.whatwg.org/multipage/media.html
     timestamp: float
     type: str
 
@@ -14,6 +16,7 @@ class DomEvent:
 @dataclass
 class PlaybackQualitySample:
     # https://developer.mozilla.org/en-US/docs/Web/API/VideoPlaybackQuality
+    # the measured data doesn't make sense at all, unused.
     samples: int
     creationTimes: list[float]
     droppedVideoFrames: list[int]
@@ -32,14 +35,3 @@ class PlaybackStatistics:
         self.playbackquality = PlaybackQualitySample(**self.playbackquality)
         for i, e in enumerate(self.events):
             self.events[i] = DomEvent.from_tuple(e)
-
-
-@dataclass
-class Test:
-    id: str
-    timestamp: int
-    events: list[DomEvent]
-
-    def __post_init__(self):
-        for i in range(len(self.events)):
-            self.events[i] = DomEvent.from_tuple(self.events[i])
