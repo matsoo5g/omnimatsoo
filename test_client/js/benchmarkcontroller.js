8205b856073b3ab23384a395b80806604b42dfcb
ChengYangWu <falau@users.noreply.github.com>
2021-06-15 00:31:36 +0200

    Parse more parameters from URL

--- test_client/js/benchmarkcontroller.js
+++ test_client/js/benchmarkcontroller.js
@@ -1,19 +1,30 @@
-const POST_BACKEND = "http://localhost:5000/collect/";
-function getVideoSrc() {
-  source = { 1: window.location.origin, 2: "http://cdn.dummy.com" };
+function prepareStatPayloadFromQueryParameters() {
+  source = {
+    1: window.location.origin,
+    2: "https://matsoo5gclient.s3.amazonaws.com",
+    3: "https://d3ovjicgaozkeh.cloudfront.net",
+  };
   vid = {
     1: "/vid/short.mp4",
-    // 2: "/vid/pprogressive.mp4",
-    // 3: "/vid/out.mp4",
-    // 4: "/vid/Dam.webm",
-    // 5: "/vid/trans_Klyuchevskoy_NG.mp4",
+    2: "/vid/bitmovin_6M_137s_hd.mp4",
+    3: "/vid/Dunkerque_2M_276s_fhd.mp4",
+    4: "/vid/Klyuchevskoy_10M_147s_4k.mp4",
+    5: "/vid/Dam_20M_24s_6k.mp4",
+    6: "/vid/Dam_10M_24s_6k.webm",
   };
   const qparams = Object.fromEntries(
     new URLSearchParams(window.location.search).entries()
   );
-  return source[qparams["s"] || 1] + vid[qparams["v"] || 1];
+  let source_string = source[qparams["s"] || 1];
+  if ("so" in qparams) {
+    source_string = qparams["so"];
+  }
+  return {
+    videoRoot: source_string + (vid[qparams["v"] || 1] || vid[1]),
+    deviceTag: qparams["dtag"] || "unknown_device",
+    postBackend: qparams["post"] || "http://localhost:5000/collect/",
+  };
 }
-
 const MediaEvents = [
   "abort",
   "canplay",
@@ -37,10 +48,11 @@ const MediaEvents = [
   "volumechange",
   "waiting",
 ];
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+
 window.addEventListener("DOMContentLoaded", () => {
   video = document.querySelector("#video");
-  video.setAttribute("src", getVideoSrc());
+  qpvars = prepareStatPayloadFromQueryParameters();
+  video.setAttribute("src", qpvars.videoRoot);
   let events = [];
   let pqEvents = {
     samples: 0,
@@ -55,16 +67,9 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  video.addEventListener("timeupdate", () => {
-    let pq = video.getVideoPlaybackQuality();
-    pqEvents.creationTimes.push(pq["creationTime"]);
-    pqEvents.totalVideoFrames.push(pq["totalVideoFrames"]);
-    pqEvents.droppedVideoFrames.push(pq["droppedVideoFrames"]);
-  });
-
   video.addEventListener("ended", () => {
     pqEvents.samples = pqEvents.creationTimes.length;
-    postData(POST_BACKEND, {
+    postData(qpvars.postBackend, {
       id:
         Math.floor(performance.now() % 0x10000).toString(16) +
         Math.floor((1 + Math.random()) * 0x1000000)
@@ -73,6 +78,7 @@ window.addEventListener("DOMContentLoaded", () => {
       timestamp: Date.now(),
       target: video.src,
       events: events,
+      device_tag: qpvars.deviceTag,
       playbackquality: pqEvents,
     });
   });
@@ -85,9 +91,7 @@ async function postData(url = "", data = {}) {
     method: "POST",
     mode: "no-cors",
     cache: "no-cache",
-    credentials: "same-origin",
     headers: { "Content-Type": "application/json" },
-    referrerPolicy: "no-referrer",
     body: JSON.stringify(data),
   });
 }
